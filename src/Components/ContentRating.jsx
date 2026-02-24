e342bba0802971e2d65da81b408623e87bd0216f
khaingthazindev <khaingthazin.dev@gmail.com>
2026-02-24 10:30:57 -0500

    added like and dislike button in ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,22 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+        likes: 0,
+        dislikes: 0
+    }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+            <p>Add text here</p>
+            <div className='rating-buttons'>
+                <button className='like-button'>Like {this.state.likes}</button>
+                <button className='like-button'>Dislike {this.state.dislikes}</button>
+            </div>
+        </div>
      </>
     );
   }
