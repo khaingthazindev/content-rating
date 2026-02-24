397a041ae2824ec01cf962581ef9d94a20c01ef0
khaingthazindev <khaingthazin.dev@gmail.com>
2026-02-24 10:47:41 -0500

    showed total ratings

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,17 +8,31 @@ class ContentRating extends Component {
 
     this.state = {
         likes: 0,
-        dislikes: 0
+        dislikes: 0,
+        totalRatings: 0,
+        handleLike: () => {
+            this.setState((prevState) => ({
+                likes: prevState.likes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }))
+        },
+        handleDislike: () => {
+            this.setState((prevState) => ({
+                dislikes: prevState.dislikes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }))
+        }
     }
   }
   render() {
     return (
      <>
         <div className='content-rating'>
-            <p>Add text here</p>
+            <h4>Content Rating App</h4>
             <div className='rating-buttons'>
-                <button className='like-button'>Like {this.state.likes}</button>
-                <button className='like-button'>Dislike {this.state.dislikes}</button>
+                <button className='like-button' onClick={this.state.handleLike}>Like {this.state.likes}</button>
+                <button className='like-button' onClick={this.state.handleDislike}>Dislike {this.state.dislikes}</button>
+                <p>Total Ratings: {this.state.totalRatings}</p>
             </div>
         </div>
      </>
